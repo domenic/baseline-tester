facc255e25508f03d261bde709210f341165b71f
Domenic Denicola <d@domenic.me>
2014-12-02 18:03:15 -0500

    Add CLI

---
+++ bin/baseline-tester.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+"use strict";
+var path = require("path");
+var baselineTester = require("..");
+var packageJson = require("../package.json");
+
+var usage = packageJson.description + "\n\n" + packageJson.name +
+            " <main-module> --cases=<folder> --input=<ext> --output=<ext> [--trim=none]";
+var argv = require("yargs")
+    .usage(usage, {
+        cases: {
+            description: "the folder in which your input and output files reside",
+            type: "string",
+            alias: "c",
+            required: true,
+            requiresArg: true
+        },
+        input: {
+            description: "the file extension for the input files",
+            type: "string",
+            alias: "i",
+            required: true,
+            requiresArg: true
+        },
+        output: {
+            description: "the file extension for the baseline output files",
+            type: "string",
+            alias: "o",
+            required: true,
+            requiresArg: true
+        },
+        trim: {
+            description: "whether to trim before comparing; \"none\" or \"both\"",
+            alias: "t",
+            requiresArg: true,
+            default: "both"
+        }
+    })
+    .require(1, "Missing required module file argument")
+    .addHelpOpt("help")
+    .version(packageJson.version, "version")
+    .argv;
+
+var func = require(path.resolve(argv._[0]));
+
+baselineTester(func, {
+    casesDirectory: argv.cases,
+    inputExtension: argv.input,
+    outputExtension: argv.output,
+    trim: argv.trim
+});
